54fe0d2a677b45e2f22dfed5766a91e508806e8c
Atul Saini <saini.atul3434@gmail.com>
2025-03-03 22:46:20 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// document
+// .getElementById("hamburger")
+// .addEventListener("click", function () {
+//   document.getElementById("nav-links").classList.toggle("open");
+// });
+
+// or
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('open');
+});
+
+
+function showCard(cardNumber) {
+  const cards = document.querySelectorAll('.content-box');
+  cards.forEach(card => {
+      card.classList.remove('active');
+  });
+  document.getElementById(`card-${cardNumber}`).classList.add('active');
+}
+
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('open');
+});
