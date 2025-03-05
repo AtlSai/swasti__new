fb3e3a2de397108942a5699184ea0bed313ce676
Atul Saini <saini.atul3434@gmail.com>
2025-03-05 23:09:13 +0530

    fifth-commit

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-// document
-// .getElementById("hamburger")
-// .addEventListener("click", function () {
-//   document.getElementById("nav-links").classList.toggle("open");
-// });
-
-// or
-
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('open');
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("open");
 });
 
-
 function showCard(cardNumber) {
-  const cards = document.querySelectorAll('.content-box');
-  cards.forEach(card => {
-      card.classList.remove('active');
+  const cards = document.querySelectorAll(".content-box");
+  cards.forEach((card) => {
+    card.classList.remove("active");
   });
-  document.getElementById(`card-${cardNumber}`).classList.add('active');
+  document.getElementById(`card-${cardNumber}`).classList.add("active");
 }
-
-
-
